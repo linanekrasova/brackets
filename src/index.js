13c90d8d4a3f282631aba4b743da3b5cc1a8cb00
linanekrasova <sumanera@yandex.ru>
2021-08-11 22:18:44 +0300

    feat: add function working with () brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+  let openBracCounter = 0;
+  if (str[0] === ")") {
+    return false;
+  }
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "(") {
+      openBracCounter++;
+    }
+    if (str[i] === ")") {
+      openBracCounter--;
+    }
+    if (openBracCounter < 0) {
+      return false;
+    }
+  }
+  if (openBracCounter === 0) {
+    return true;
+  }
+};
