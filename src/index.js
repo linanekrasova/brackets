3170a0874a800a0d1a61e02090464508306dd95a
linanekrasova <sumanera@yandex.ru>
2021-08-12 00:36:11 +0300

    fix: add external loop

--- src/index.js
+++ src/index.js
@@ -3,20 +3,21 @@ module.exports = function check(str, bracketsConfig) {
     return false;
   }
   let openBracCounter = 0;
-  if (str[0] === ")") {
-    return false;
+
+  for (let j = 0; j < bracketsConfig.length; j++) {
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === bracketsConfig[j][0]) {
+        openBracCounter++;
+      }
+      if (str[i] === bracketsConfig[j][1]) {
+        openBracCounter--;
+      }
+      if (openBracCounter < 0) {
+        return false;
+      }
+    }
   }
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] === "(") {
-      openBracCounter++;
-    }
-    if (str[i] === ")") {
-      openBracCounter--;
-    }
-    if (openBracCounter < 0) {
-      return false;
-    }
-  }
+
   if (openBracCounter === 0) {
     return true;
   }
