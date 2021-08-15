15be919c73e13f55afd6da9aa0b39cdb8c1d83ef
linanekrasova <sumanera@yandex.ru>
2021-08-15 19:54:29 +0300

    fix: add fix for test №5

--- src/index.js
+++ src/index.js
@@ -2,23 +2,28 @@ module.exports = function check(str, bracketsConfig) {
   if (str.length % 2 !== 0) {
     return false;
   }
-  let openBracCounter = 0;
+  let bracCounter = 0;
+  let nextCloseBreakType = [];
 
-  for (let j = 0; j < bracketsConfig.length; j++) {
-    for (let i = 0; i < str.length; i++) {
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
       if (str[i] === bracketsConfig[j][0]) {
-        openBracCounter++;
+        bracCounter++;
+        nextCloseBreakType.push(bracketsConfig[j][1]);
       }
       if (str[i] === bracketsConfig[j][1]) {
-        openBracCounter--;
+        if (str[i] !== nextCloseBreakType[nextCloseBreakType.length - 1]) {
+          return false;
+        } else {
+          nextCloseBreakType.pop();
+        }
+        bracCounter--;
       }
-      if (openBracCounter < 0) {
+      if (bracCounter < 0) {
         return false;
       }
     }
   }
 
-  if (openBracCounter === 0) {
-    return true;
-  }
+  return bracCounter === 0;
 };
